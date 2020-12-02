73ef00dd61b08b0d257c7651b6545ee08dd059d1
Yevhen Fastiuk <yevfast@gmail.com>
2020-12-02 11:31:44 +0200

    Align UI

--- lab1/mainwindow.cpp
+++ lab1/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "sha.h"
+#include <cstring>
 
 using namespace std;
 
@@ -18,5 +19,17 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
+    uint8_t sha[SHASUM_SIZE];
+    char sha_str[SHASUM_SIZE * 2 + 1];
+    QString text = ui->textEdit->toPlainText();
 
+    if (sha1_calculate(text.toStdString().c_str(), text.toStdString().length(),
+                       sha, sizeof(sha)) == 0) {
+        for (uint i = 0; i < SHASUM_SIZE; ++i) {
+            sprintf(&sha_str[i * 2], "%02x", sha[i]);
+        }
+        ui->textEdit_2->setText(QString::fromStdString(string(sha_str)));
+    } else {
+        ui->textEdit_2->setText(QString::fromStdString(string("SHA ERROR")));
+    }
 }
