dd3e627139f4862aaa32dcad5404fd9ac59ddd8c
Yevhen Fastiuk <yevfast@gmail.com>
2021-01-06 02:51:59 +0200

    Integrate DSA library into test app

--- lab2/mainwindow.cpp
+++ lab2/mainwindow.cpp
@@ -1,18 +1,21 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <cstring>
 
 extern "C" {
 #include "dsa.h"
 }
 
+using namespace std;
+
+static dsa_param_t param;
+static dsa_keypair_t keypair;
+static dsa_signature_t signature;
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    dsa_param_t par;
-    dsa_keypair_t kp;
-    dsa_signature_t sign;
-
     ui->setupUi(this);
 
     if (dsa_self_test()) {
@@ -22,20 +25,76 @@ MainWindow::MainWindow(QWidget *parent)
     }
 
     dsa_init();
-    dsa_generate_param(&par);
-    dsa_generate_keypair(&par, &kp);
-    dsa_sign(&par, &kp, "Hello", 5, &sign);
-    if (dsa_validate(&par, &kp, "Hello", 5, &sign)) {
-        printf("DSA signature invalid\n");
-    } else {
-        printf("DSA signature is valid\n");
-    }
-
-    dsa_destroy();
 }
 
 MainWindow::~MainWindow()
 {
+    dsa_destroy();
     delete ui;
 }
 
+void MainWindow::on_pushButton_generate_params_clicked()
+{
+    dsa_generate_param(&param);
+    dsa_generate_keypair(&param, &keypair);
+
+    ui->textEdit_param_p->setText(QString::fromStdString(string(param.p)));
+    ui->textEdit_param_q->setText(QString::fromStdString(string(param.q)));
+    ui->textEdit_param_g->setText(QString::fromStdString(string(param.g)));
+    ui->textEdit_priv_key->setText(QString::fromStdString(string(keypair.x)));
+    ui->textEdit_publ_key->setText(QString::fromStdString(string(keypair.y)));
+
+    ui->textEdit_param_r->setText(QString::fromStdString(string("")));
+    ui->textEdit_signature->setText(QString::fromStdString(string("")));
+    ui->label_validation_result->setText(QString::fromStdString(string("")));
+}
+
+void MainWindow::on_pushButton_sign_clicked()
+{
+    dsa_sign(&param, &keypair,
+             ui->textEdit_msg->toPlainText().toStdString().c_str(),
+             ui->textEdit_msg->toPlainText().toStdString().length(),
+             &signature);
+
+    ui->textEdit_param_r->setText(QString::fromStdString(string(signature.r)));
+    ui->textEdit_signature->setText(QString::fromStdString(string(signature.s)));
+
+    ui->label_validation_result->setText(QString::fromStdString(string("")));
+}
+
+void MainWindow::on_pushButton_validate_clicked()
+{
+    int res;
+
+    // For testing purposes
+    strncpy(param.p, ui->textEdit_param_p->toPlainText().toStdString().c_str(),
+            sizeof(param.p));
+    strncpy(param.q, ui->textEdit_param_q->toPlainText().toStdString().c_str(),
+            sizeof(param.q));
+    strncpy(param.g, ui->textEdit_param_g->toPlainText().toStdString().c_str(),
+            sizeof(param.g));
+    strncpy(keypair.x,
+            ui->textEdit_priv_key->toPlainText().toStdString().c_str(),
+            sizeof(keypair.x));
+    strncpy(keypair.y,
+            ui->textEdit_publ_key->toPlainText().toStdString().c_str(),
+            sizeof(keypair.y));
+    strncpy(signature.r,
+            ui->textEdit_param_r->toPlainText().toStdString().c_str(),
+            sizeof(signature.r));
+    strncpy(signature.s,
+            ui->textEdit_signature->toPlainText().toStdString().c_str(),
+            sizeof(signature.s));
+
+    res = dsa_validate(&param, &keypair,
+                       ui->textEdit_msg->toPlainText().toStdString().c_str(),
+                       ui->textEdit_msg->toPlainText().toStdString().length(),
+                       &signature);
+    if (res) {
+        ui->label_validation_result->setText(
+                       QString::fromStdString(string("DSA signature invalid")));
+    } else {
+        ui->label_validation_result->setText(
+                      QString::fromStdString(string("DSA signature is valid")));
+    }
+}
--- lab2/mainwindow.h
+++ lab2/mainwindow.h
@@ -15,6 +15,13 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_generate_params_clicked();
+
+    void on_pushButton_sign_clicked();
+
+    void on_pushButton_validate_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
